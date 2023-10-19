ca189d77f2747a9216f2df2a59e479f9cbc65376
ljlVink <qpalzmlong@foxmail.com>
2023-10-19 21:25:45 +0800

    delete old fils

--- assets/js/9ead8d56.ec5b95a7.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkapidemo=self.webpackChunkapidemo||[]).push([[431],{5745:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
--- readme.md
+++
@@ -1,7 +0,0 @@
-# lspdemo
-
-LinspirerDemo Release Build
-
-1.Official site
-
-2.Release apks
